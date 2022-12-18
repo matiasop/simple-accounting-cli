6a346cd193026f46ef8e95dd8972d60f7f594221
matiasop <movalle4@uc.cl>
2022-12-17 21:27:21 -0300

    chore(prisma): add prisma initial config

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,38 @@
 "use strict";
+var __async = (__this, __arguments, generator) => {
+  return new Promise((resolve, reject) => {
+    var fulfilled = (value) => {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    };
+    var rejected = (value) => {
+      try {
+        step(generator.throw(value));
+      } catch (e) {
+        reject(e);
+      }
+    };
+    var step = (x) => x.done ? resolve(x.value) : Promise.resolve(x.value).then(fulfilled, rejected);
+    step((generator = generator.apply(__this, __arguments)).next());
+  });
+};
 
 // src/app.ts
-console.log("hello world");
+var import_client = require("@prisma/client");
+var prisma = new import_client.PrismaClient();
+function main() {
+  return __async(this, null, function* () {
+    const users = yield prisma.user.findMany();
+    console.log(users);
+  });
+}
+main().then(() => __async(exports, null, function* () {
+  yield prisma.$disconnect();
+})).catch((e) => __async(exports, null, function* () {
+  console.log(e);
+  yield prisma.$disconnect();
+  process.exit(1);
+}));
